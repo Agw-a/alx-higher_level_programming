6ebbe01416fb03e0baae56c2c3aa1f8b7c4a1bbf
Agw-a <lizagwa7@gmail.com>
2022-06-01 01:30:12 -0700

    Adding forst tasks

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lastDigit = abs(number) % 10
+out_put = "Last digit of"
+if number < 0:
+    lastDigit = -lastDigit
+print("{}{} is {} and is ".format(out_put, number, lastDigit), end="")
+if lastDigit > 5:
+    print("graeter than 5")
+if lastDigit == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
