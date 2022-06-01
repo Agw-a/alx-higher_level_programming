269ada111ac51d76069ab98742fb0494b8569f86
Agw-a <lizagwa7@gmail.com>
2022-06-01 07:22:31 -0700

    Betty syncing

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -5,43 +5,39 @@
  * @head:address of the head of the list
  * @number:number to beadded to list
  * Return:address of new node
- * Description:a function in C that inserts a number into a 
+ * Description:a function in C that inserts a number into a
  * sorted singly linked list.
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *newnode, *previousnode, *tempnode;
+	listint_t *newnode, *previousnode, *tempnode;
 
-    if (!head)
-    return (NULL);
+	if (!head)
+	return (NULL);
 
-    newnode = malloc(sizeof(listint_t));
-    if (!newnode)
-    return (NULL);
-    newnode->n = number;
-    newnode->next = NULL;
-
-    if (!*head)
-    {
-        *head = newnode;
-        return (newnode);
-
-    }
-    tempnode = *head;
-    if ((*head)->n > number)
-    {
-        newnode->next = *head;
-        *head = newnode;
-        return (newnode);
-    }
-    while (tempnode && (tempnode->n < number))
-    {
-        previousnode = tempnode;
-        tempnode = tempnode->next;
-    }
-    newnode->next = tempnode;
-    previousnode->next = newnode;
-    return (newnode);
-    
-    
+	newnode = malloc(sizeof(listint_t));
+	if (!newnode)
+	return (NULL);
+	newnode->n = number;
+	newnode->next = NULL;
+	if (!*head)
+	{
+		*head = newnode;
+		return (newnode);
+	}
+	tempnode = *head;
+	if ((*head)->n > number)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
+	while (tempnode && (tempnode->n < number))
+	{
+		previousnode = tempnode;
+		tempnode = tempnode->next;
+	}
+	newnode->next = tempnode;
+	previousnode->next = newnode;
+	return (newnode);
 }
