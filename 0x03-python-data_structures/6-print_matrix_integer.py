6cff66fcabf614fa8c791d28938d2f882db6bce0
Agw-a <lizagwa7@gmail.com>
2022-06-06 12:56:58 -0700

    Changing method, correct output

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for x in matrix:
-        for y in range(0, len(x)):
-            print("{:d}".format(x[y], end=""))
-            if y != len(x) - 1:
-                print(" ", end="")
-        print("")
+    for component in matrix:
+        if isinstance(component, list):
+            if len(component) == 0:
+                print("")
+            for element in component:
+                if component.index(element) == len(component) - 1:
+                    print("{:d}".format(element))
+                else:
+                    print("{:d} ".format(element), end="")
+        else:
+            print("{:d}".format(component))
