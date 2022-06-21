bc79b8ea9ead89fb6b4704d324d21d049516499b
Agw-a <lizagwa7@gmail.com>
2022-06-21 10:04:33 +0000

    1 - 7

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for lst in range(x):
+        try:
+            print("{:d}".format(my_list[lst]), end="")
+            count += 1
+        except(ValueError, TypeError):
+            pass
+    print()
+    return count
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+
+    n = 0
+    new_list = []
+
+    while n < list_length:
+        result = 0
+        try:
+            result = my_list_1[n] / my_list_2[n]
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            new_list.append(result)
+            n += 1
+    return new_list
