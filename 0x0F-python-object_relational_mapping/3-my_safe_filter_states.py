72ce5f4e2a8b95bcd0a496da812f2a8060096b68
Agw-a <lizagwa7@gmail.com>
2022-09-15 08:35:06 +0000

    Task 3:

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-accepts 3 arguments (mysql username, password and database name)
+receive 3 arguments: mysql username, password and database name
 and lists all states from that database whose names start with a given string
 SAFELY
 """
@@ -9,7 +9,7 @@ import MySQLdb
 
 
 def main(argv):
-    """connects to a given mysql database and lists filtered states from it"""
+    """connects to a mysql database and lists filtered states from it"""
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
