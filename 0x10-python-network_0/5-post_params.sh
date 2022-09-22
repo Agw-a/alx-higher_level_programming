e9f0529d095f5dbf969220e9c624c53762479b66
Agw-a <lizagwa7@gmail.com>
2022-09-22 07:07:09 -0700

    Task 5:2

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #a Bash script that takes in a URL, sends a POST request to the passed URL, and
 #displays the body of the response
-curl -sLX POST "$1" -d "email="
+curl -sLX POST "$1" -d "email=test%40%gmail2Ecom&subject=I+will+always+be+here+for+PLD"
