679db356b79bf30288692d11bc91b1b4d0d6f27b
Agw-a <lizagwa7@gmail.com>
2022-10-26 12:26:54 -0700

    Advanced tasts

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	$('DIV#add_item').on('click', function (e) {
+	$('UL.my_list').append('<li>Item</li>')
+	});
+	 $('DIV#remove_item').on('click', function (e) {
+        $('UL.my_list li:last-child').last().remove();
+        });
+	$('DIV#clear_list').on('click', function (e) {
+        $('UL.my_list').empty();
+        });
+
+});
